b1e07706a47eb45624b2e251ebb72fb9df56c00b
JDurham95 <jacobgdurham@gmail.com>
2025-11-01 10:37:15 -0600

    added code so the passed in file would be cleared after it was read. Added comments throughout

--- save_text_file.py
+++ save_text_file.py
@@ -9,14 +9,26 @@ def save_text_file(file):
     The user will pass a text file. The first line of the file indicates either append ("a") or write ("w").
     The second line of the file is the text to be saved. The third line of the file is the name to be saved
     """
+
+    #open the file, get the individual lines as the content array. clear the contents of the passed in file.
     with open(file, "r") as f:
         content = f.readlines()
+    open(file, "w").close()
+
+    #the first line is action to be taken, either append or write
     action = content[0]
+
+    #the third line is the name of the file
     if content[2]:
         name = content[2]
+
+        #append ".txt" if needed
+        if name[-4:] != ".txt":
+            name = name + ".txt"
     else:
         name = "temp.txt"
 
+    #append if the action is "a" and the name already exists, otherwise write
     if action == "a" and os.path.exists(name):
         with open(name, "a", encoding="utf-8") as f:
             f.write(content[1])
